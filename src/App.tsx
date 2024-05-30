1d2bafd66899ffe9eeb09b95f51c5809a6c5d6aa
Ronal Rojas <ronalrojasroquej@gmail.com>
2024-05-29 20:49:26 -0500

    update liks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Home from './pages/home/home';
 import './styles/index.css';
 import ErrorPage from './components/errorpage/ErrorPage';
 
 function App() {
+  const location = useLocation();
   
+  React.useEffect(() => {
+    if (location.pathname === '/voyagesdemo') {
+      window.location.href = '/voyagesdemo/home';
+    }
+  }, [location.pathname]);
   return (
     <div>
       <Routes>
-        <Route path="/voyagesdemo/" element={<Home />} />
+        <Route path="/voyagesdemo" element={<Home />} />
         <Route path="/voyagesdemo/home" element={<Home />} />
         <Route path="/voyagesdemo/error" element={<ErrorPage />} />
-        <Route path="/*" element={<Navigate to="/error" />} />
+        <Route path="/voyagesdemo/*" element={<Navigate to="/error" />} />
       </Routes>
     </div>
   );
